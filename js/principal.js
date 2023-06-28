d4888bbbcbcbf932c37f81e6d16c5604a09d5fba
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-06-28 10:30:11 -0600

    update

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
-//var bienvenida=document.querySelector('#bienvenida');
+var bienvenida=document.querySelector('#bienvenida');
 let user = localStorage.getItem("user");
 let saldo = localStorage.getItem("saldo");
-//bienvenida.textContent="Bienvenido "+ user + " Saldo "+saldo ;
+bienvenida.textContent="Bienvenid@ "+ user;
